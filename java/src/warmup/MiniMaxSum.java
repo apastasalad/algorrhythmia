90ffa9b9a382b25ef9f7188a91dfe02958dbd871
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-24 16:04:32 +1100

    Update formatting

--- java/src/warmup/MiniMaxSum.java
+++ java/src/warmup/MiniMaxSum.java
@@ -3,13 +3,14 @@ package warmup;
 import java.math.BigInteger;
 
 public class MiniMaxSum {
-// arr [1,3,5,7,9]
-// The minimum sum is 16 and the maximum sum is 24. The function prints
-//16 24
 
+    // arr [1,3,5,7,9]
+    // The minimum sum is 16 and the maximum sum is 24. The function prints
+    //16 24
     static void miniMaxSum(int[] arr) {
 
         // keep track of the highest and lowest seen
+        // this is ugly, but I do not know how to deal with the equivalent of Integer.MAX_VALUE
         BigInteger lowestSum = BigInteger.valueOf(9999999999L);
         BigInteger highestSum = BigInteger.valueOf(0L);
         BigInteger currentSum;
