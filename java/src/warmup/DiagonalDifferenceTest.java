461307fc2f83b4497d3efc66d9447c4e4764b8a5
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-24 14:01:12 +1100

    Remove redundant declaration

--- java/src/warmup/DiagonalDifferenceTest.java
+++ java/src/warmup/DiagonalDifferenceTest.java
@@ -14,10 +14,10 @@ class DiagonalDifferenceTest {
 
         // This is really ugly but I don't know how else to do it.
         List<List<Integer>> arr = new ArrayList<>();
-        
-        arr.add(0, Arrays.asList(new Integer[] {11,2,4}));
-        arr.add(1, Arrays.asList(new Integer[] {4,5,6}));
-        arr.add(2, Arrays.asList(new Integer[] {10,8,-12}));
+        arr.add(0, Arrays.asList(11,2,4));
+        arr.add(1, Arrays.asList(4,5,6));
+        arr.add(2, Arrays.asList(10,8,-12));
+
         int want = 15;
         int got =  DiagonalDifference.diagonalDifference(arr);
 
