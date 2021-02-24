ca4bc846529991aa09b25ac8fade277fe131a8a1
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-24 17:05:51 +1100

    Add the TimeConversion Algoirthms

---
+++ java/src/warmup/TimeConversion.java
@@ -0,0 +1,49 @@
+package warmup;
+
+import java.util.StringTokenizer;
+
+public class TimeConversion {
+
+    /*
+     * Complete the timeConversion function below.
+     */
+    static String timeConversion(String s) {
+        StringBuilder sb = new StringBuilder();
+
+        // tokenize and remove the AM / PM field
+        String trimmedString = s.substring(0, s.length()-2);
+        StringTokenizer st = new StringTokenizer(trimmedString, ":");
+
+        // Collect the time elements.
+        String hours = st.nextToken();
+        String minutes = st.nextToken();
+        String seconds = st.nextToken();
+
+        // Look at the hours field
+        if (s.contains("AM")) {
+            if (hours.equals("12")) {
+                // 12AM is a special case
+                sb.append("00");
+            } else {
+                sb.append(hours);
+            }
+        } else {
+            if (hours.equals("12")) {
+                // 12PM is also special case
+                sb.append("12");
+            } else {
+                sb.append(Integer.parseInt(hours) + 12);
+            }
+        }
+
+        // Add the minutes field
+        sb.append(":");
+        sb.append(minutes);
+        sb.append(":");
+
+        // Add the seconds field
+        sb.append(seconds);
+
+        return sb.toString();
+    }
+}
---
+++ java/src/warmup/TimeConversionTest.java
@@ -0,0 +1,45 @@
+package warmup;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.StringTokenizer;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TimeConversionTest {
+
+    @Test
+    void timeConversionTestOne() {
+
+        String testTime = "07:05:45PM";
+        String want = "19:05:45";
+        String got = TimeConversion.timeConversion(testTime);
+
+        Assertions.assertEquals(want, got);
+    }
+
+    @Test
+    void timeConversionTestTwo() {
+
+        String testTime = "12:40:22AM";
+        String want = "00:40:22";
+        String got = TimeConversion.timeConversion(testTime);
+
+        Assertions.assertEquals(want, got);
+    }
+
+    @Test
+    void timeConversionTestThree() {
+
+        String testTime = "12:45:54PM";
+        String want = "12:45:54";
+        String got = TimeConversion.timeConversion(testTime);
+
+        Assertions.assertEquals(want, got);
+    }
+
+
+
+
+}
