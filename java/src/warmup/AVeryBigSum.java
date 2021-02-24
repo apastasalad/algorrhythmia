fbb1a220bfbe419a2db0bba9ecd69efaeadd12ff
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-24 13:07:28 +1100

    Add AVeryBigSum Algorithm

---
+++ java/src/warmup/AVeryBigSum.java
@@ -0,0 +1,18 @@
+package warmup;
+
+public class AVeryBigSum {
+
+    // Calculate and print the sum of the elements in an array,
+    // keeping in mind that some of those integers may be quite large.
+    static long aVeryBigSum(long[] ar) {
+
+        long result = 0;
+
+        for (int i = 0; i < ar.length; i++) {
+
+            result += ar[i];
+        }
+
+        return result;
+    }
+}
---
+++ java/src/warmup/AVeryBigSumTest.java
@@ -0,0 +1,17 @@
+package warmup;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class AVeryBigSumTest {
+
+    @Test
+    void aVeryBigSum() {
+
+        long[] testArray = {1000000001,1000000002,1000000003,1000000004,1000000005};
+        long want = 5000000015l;
+        long got = AVeryBigSum.aVeryBigSum(testArray);
+
+        Assertions.assertEquals(want, got);
+    }
+}
