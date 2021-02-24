5b37c3f548ac83daa1f1c8a17c1cc6c67f68cdf2
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-24 14:29:47 +1100

    Add the Staircase Algorithm

---
+++ java/src/warmup/Staircase.java
@@ -0,0 +1,31 @@
+package warmup;
+
+public class Staircase {
+
+    // Create a staircase
+    static void staircase(int n) {
+
+        int spaceCounter = n - 1;
+        int stepCounter = 1;
+        StringBuffer sb = new StringBuffer();
+
+        for (int i = 0; i < n; i++) {
+            // Print the spaces
+            for (int j = 0; j < spaceCounter; j++) {
+                sb.append(" ");
+            }
+
+            // Print the hashes
+            for (int k = 0; k < stepCounter; k++) {
+                sb.append("#");
+            }
+
+            // add a newline, and calculate the next step
+            sb.append(System.getProperty("line.separator"));
+
+            spaceCounter--;
+            stepCounter++;
+        }
+        System.out.println(sb.toString());
+    }
+}
---
+++ java/src/warmup/StaircaseTest.java
@@ -0,0 +1,11 @@
+package warmup;
+
+import org.junit.jupiter.api.Test;
+
+class StaircaseTest {
+
+    @Test
+    void staircase() {
+        Staircase.staircase(22);
+    }
+}
