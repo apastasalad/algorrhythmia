d645b49af0229520d778ecd8003880de6992f6c6
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-25 21:49:47 +1100

    Reformate code

--- java/src/implementation/DivisibleSumPairsTest.java
+++ java/src/implementation/DivisibleSumPairsTest.java
@@ -1,7 +1,5 @@
 package implementation;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -12,7 +10,7 @@ class DivisibleSumPairsTest {
 
     int[] testArray = {1, 3, 2, 6, 1, 2};
     int want = 5;
-    int got = DivisibleSumPairs.divisibleSumPairs(6,3, testArray);
+    int got = DivisibleSumPairs.divisibleSumPairs(6, 3, testArray);
 
     Assertions.assertEquals(want, got);
   }
@@ -22,7 +20,7 @@ class DivisibleSumPairsTest {
 
     int[] testArray = {1, 2, 3, 4, 5, 6};
     int want = 3;
-    int got = DivisibleSumPairs.divisibleSumPairs(6,5, testArray);
+    int got = DivisibleSumPairs.divisibleSumPairs(6, 5, testArray);
 
     Assertions.assertEquals(want, got);
   }
@@ -30,9 +28,13 @@ class DivisibleSumPairsTest {
   @Test
   void divisibleSumPairsTestThree() {
 
-    int[] testArray = {43,95,51,55,40,86,65,81,51,20,47,50,65,53,23,78,75,75,47,73,25,27,14,8,26,58,95,28,3,23,48,69,26,3,73,52,34,7,40,33,56,98,71,29,70,71,28,12,18,49,19,25,2,18,15,41,51,42,46,19,98,56,54,98,72,25,16,49,34,99,48,93,64,44,50,91,44,17,63,27,3,65,75,19,68,30,43,37,72,54,82,92,37,52,72,62,3,88,82,71};
+    int[] testArray = {43, 95, 51, 55, 40, 86, 65, 81, 51, 20, 47, 50, 65, 53, 23, 78, 75, 75, 47,
+        73, 25, 27, 14, 8, 26, 58, 95, 28, 3, 23, 48, 69, 26, 3, 73, 52, 34, 7, 40, 33, 56, 98, 71,
+        29, 70, 71, 28, 12, 18, 49, 19, 25, 2, 18, 15, 41, 51, 42, 46, 19, 98, 56, 54, 98, 72, 25,
+        16, 49, 34, 99, 48, 93, 64, 44, 50, 91, 44, 17, 63, 27, 3, 65, 75, 19, 68, 30, 43, 37, 72,
+        54, 82, 92, 37, 52, 72, 62, 3, 88, 82, 71};
     int want = 216;
-    int got = DivisibleSumPairs.divisibleSumPairs(100,22, testArray);
+    int got = DivisibleSumPairs.divisibleSumPairs(100, 22, testArray);
 
     Assertions.assertEquals(want, got);
   }
--- java/src/implementation/MigratoryBirdsTest.java
+++ java/src/implementation/MigratoryBirdsTest.java
@@ -1,7 +1,5 @@
 package implementation;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.Arrays;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -10,7 +8,7 @@ class MigratoryBirdsTest {
 
   @Test
   void migratoryBirdsTestOne() {
-    Integer[] testArray = {1,4,4,4,5,3};
+    Integer[] testArray = {1, 4, 4, 4, 5, 3};
     int want = 4;
     int got = MigratoryBirds.migratoryBirds(Arrays.asList(testArray));
 
@@ -19,14 +17,12 @@ class MigratoryBirdsTest {
 
   @Test
   void migratoryBirdsTestTwo() {
-    Integer[] testArray = {1,2,3,4,5,4,3,2,1,3,4};
+    Integer[] testArray = {1, 2, 3, 4, 5, 4, 3, 2, 1, 3, 4};
     int want = 3;
     int got = MigratoryBirds.migratoryBirds(Arrays.asList(testArray));
 
     Assertions.assertEquals(want, got);
   }
-
-
 
 
 }
