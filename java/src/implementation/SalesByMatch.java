8ba939b76740e649bfcb26fba9c3b3a22669969d
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-27 15:45:16 +1100

    add sockMerchant logic

--- java/src/implementation/SalesByMatch.java
+++ java/src/implementation/SalesByMatch.java
@@ -1,7 +1,6 @@
 package implementation;
 
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 
 public class SalesByMatch {
@@ -13,13 +12,19 @@ public class SalesByMatch {
     int result = 0;
 
     // Sort all the socks
+    for (int sock : ar) {
+      // if we've already seen this sock before, increment the counter,
+      // otherwise add a new sock
+      if (sockMap.containsKey(sock)) {
+        sockMap.put(sock, (sockMap.get(sock) + 1));
+      } else {
+        sockMap.put(sock, 1);
+      }
+    }
 
     // Count the pairs only
-    Iterator sockItr = sockMap.entrySet().iterator();
-    while (sockItr.hasNext()) {
-      if (((Integer) sockItr.next()) % 2 == 0) {
-        result++;
-      }
+    for (Integer sock : sockMap.keySet()) {
+      result+= sockMap.get(sock) / 2;
     }
 
     // Return the number of pairs
