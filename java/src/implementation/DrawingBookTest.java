1a6fa481b847b59436f499ad1876115bace6efdd
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-27 18:29:01 +1100

    Add DrawingBook algorithm

--- java/src/implementation/DrawingBookTest.java
+++ java/src/implementation/DrawingBookTest.java
@@ -25,4 +25,39 @@ class DrawingBookTest {
     int got = DrawingBook.pageCount(5,5);
     Assertions.assertEquals(want, got);
   }
+
+  @Test
+  void pageCountTestFour() {
+    int want = 0;
+    int got = DrawingBook.pageCount(5,1);
+    Assertions.assertEquals(want, got);
+  }
+
+  @Test
+  void pageCountTestFive() {
+    int want = 1;
+    int got = DrawingBook.pageCount(7,3);
+    Assertions.assertEquals(want, got);
+  }
+
+  @Test
+  void pageCountTestSix() {
+    int want = 0;
+    int got = DrawingBook.pageCount(5,4);
+    Assertions.assertEquals(want, got);
+  }
+
+  @Test
+  void pageCountTestSeven() {
+    int want = 1;
+    int got = DrawingBook.pageCount(7,4);
+    Assertions.assertEquals(want, got);
+  }
+
+  @Test
+  void pageCountTestEight() {
+    int want = 8720;
+    int got = DrawingBook.pageCount(95073,17440);
+    Assertions.assertEquals(want, got);
+  }
 }
