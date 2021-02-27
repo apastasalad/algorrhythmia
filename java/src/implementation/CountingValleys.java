b71a3adfc55f0bebf5f52df22dfc108fe8284e16
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-02-27 18:34:12 +1100

    Add the CountingValleys algorithm

---
+++ java/src/implementation/CountingValleys.java
@@ -0,0 +1,16 @@
+package implementation;
+
+public class CountingValleys {
+
+  /*
+   * Count the Number of times the hiker goes below sea level, and returns.
+   */
+  public static int countingValleys(int steps, String path) {
+    // Write your code here
+
+    return -1;
+
+  }
+
+
+}
