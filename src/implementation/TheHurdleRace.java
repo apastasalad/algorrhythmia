f695cb76b61b33d6480048bfeff77196b25af086
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-01 13:59:04 +1100

    finish the HurdleRace

--- src/implementation/TheHurdleRace.java
+++ src/implementation/TheHurdleRace.java
@@ -5,9 +5,18 @@ public class TheHurdleRace {
   // For a given height, and a number of hurdles, how many potions to take?
   static int hurdleRace(int k, int[] height) {
 
-    return -1;
+    int jumpHeight = k;
+    int potionCounter = 0;
 
+    for (int hurdleHeight : height) {
+      if (hurdleHeight > jumpHeight) {
+        // how many potions to take?
+        int takeSomePotion = hurdleHeight - jumpHeight;
+        potionCounter+= takeSomePotion;
+        jumpHeight+= takeSomePotion;
+      }
+    }
 
+    return potionCounter;
   }
-
 }
