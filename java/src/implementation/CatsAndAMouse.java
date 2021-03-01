91b5ae2972b7a1c6c7048e9099343444d29da694
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-01 11:43:55 +1100

    Add CatsAndAMouse algorithm - initial checkin

---
+++ java/src/implementation/CatsAndAMouse.java
@@ -0,0 +1,10 @@
+package implementation;
+
+public class CatsAndAMouse {
+
+  static String catAndMouse(int catA, int catB, int mouse) {
+
+    return "FAIL";
+
+  }
+}
---
+++ java/src/implementation/CatsAndAMouseTest.java
@@ -0,0 +1,25 @@
+package implementation;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class CatsAndAMouseTest {
+
+  @Test
+  void catAndMouseTestOne() {
+
+    String want = "Cat B";
+    String got = CatsAndAMouse.catAndMouse(1,2,3);
+
+    Assertions.assertEquals(want, got);
+  }
+
+  @Test
+  void catAndMouseTestTwo() {
+
+    String want = "Mouse C";
+    String got = CatsAndAMouse.catAndMouse(1,3,2);
+
+    Assertions.assertEquals(want, got);
+  }
+}
