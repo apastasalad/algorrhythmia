d6e9cb26fc84faa22120d7b71aca6b91da39a721
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-01 11:29:05 +1100

    Final ElectronicsShop algorithm

--- java/src/implementation/ElectronicsShop.java
+++ java/src/implementation/ElectronicsShop.java
@@ -1,5 +1,9 @@
 package implementation;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class ElectronicsShop {
 
   /*
@@ -7,7 +11,23 @@ public class ElectronicsShop {
    */
   static int getMoneySpent(int[] keyboards, int[] drives, int b) {
 
+    List <Integer> combinations = new ArrayList<>();
 
-    return -1;
+    // populate all the purchase combinations with their combined cost;
+    for (int keyboard : keyboards) {
+      for (int drive : drives) {
+        if (b - keyboard - drive >= 0) {
+          combinations.add(keyboard + drive);
+        }
+      }
+    }
+
+    // if there are no combinations, then return -1, otherwise return the most expensive combination
+    // that can be bought.
+    if (combinations.isEmpty()) {
+      return -1;
+    } else {
+      return Collections.max(combinations);
+    }
   }
 }
