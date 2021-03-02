db384d86cf481136f2114199fc1d7362c842ec1c
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-02 21:24:41 +1100

    Climbing Leaderboard algorithm

--- src/implementation/LeaderBoard.java
+++ src/implementation/LeaderBoard.java
@@ -1,34 +1,38 @@
 package implementation;
 
 import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Set;
 
 public class LeaderBoard {
 
+
   public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-    List<Integer> results = new ArrayList<>();
-    List<Integer> rankedLeaderBoard = new ArrayList<>(ranked);
 
-    for (int i = 0; i < player.size(); i++) {
+    // Use a LinkedHashSet to maintain an ordered set.
+    Set<Integer> tempSet = new LinkedHashSet<>(ranked);
+    List<Integer> rankedLeaderBoard = new ArrayList<>(tempSet);
+    List<Integer> results = new LinkedList<>();
 
-      // add the new score. Use a set so that the index is the rank
-      rankedLeaderBoard.add(player.get(i));
-      Set<Integer> tempSet = new HashSet<>(rankedLeaderBoard);
-      rankedLeaderBoard = new ArrayList<>(tempSet);
+    // start with a rank beyond the dimensions of the set
+    int rank = rankedLeaderBoard.size() + 1;
 
-      // sort the array
-      Collections.sort(rankedLeaderBoard, Collections.reverseOrder());
-
-      // locate the rank and add it
-      int index = Collections.binarySearch(rankedLeaderBoard, player.get(i), Collections.reverseOrder());
-      results.add(index + 1);
-
-      rankedLeaderBoard.remove(index);
+    // Work from the end to the beginning looking for the correct rank location.
+    for (Integer i : player) {
+      for (int j = rank - 2; j >= 0; j--) {
+        if (i < rankedLeaderBoard.get(j)) {         // the score is < than the next leaderboard score
+          break;
+        } else if (i.equals(rankedLeaderBoard.get(j))) { // the score is = to the leaderboard score
+          rank--;
+          break;
+        } else {                                    // the score is > the leaderboard score
+          rank--;
+        }
+      }
+      results.add(rank);
     }
-
     return results;
   }
 }
