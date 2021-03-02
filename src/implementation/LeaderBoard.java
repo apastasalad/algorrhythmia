b714409a2a366821e49900303353a4102d3ed078
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-02 20:50:23 +1100

    Climbing Leaderboard algorithm

--- src/implementation/LeaderBoard.java
+++ src/implementation/LeaderBoard.java
@@ -1,51 +1,32 @@
 package implementation;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class LeaderBoard {
 
   public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-
     List<Integer> results = new ArrayList<>();
+    List<Integer> rankedLeaderBoard = new ArrayList<>(ranked);
 
     for (int i = 0; i < player.size(); i++) {
 
-      // clear everything and begin
-      int rankCounter = 1;
-      boolean found = false;
-      Integer[] rankedLeaderBoard = {};
+      // add the new score. Use a set so that the index is the rank
+      rankedLeaderBoard.add(player.get(i));
+      Set<Integer> tempSet = new HashSet<>(rankedLeaderBoard);
+      rankedLeaderBoard = new ArrayList<>(tempSet);
 
-      // add all the current scores to the array
-      rankedLeaderBoard = ranked.toArray(rankedLeaderBoard);
+      // sort the array
+      Collections.sort(rankedLeaderBoard, Collections.reverseOrder());
 
-      // add the new score and sort the array
-      rankedLeaderBoard = Arrays.copyOf(rankedLeaderBoard, rankedLeaderBoard.length + 1);
-      rankedLeaderBoard[rankedLeaderBoard.length - 1] = player.get(i);
-      Arrays.sort(rankedLeaderBoard, Collections.reverseOrder());
+      // locate the rank and add it
+      int index = Collections.binarySearch(rankedLeaderBoard, player.get(i), Collections.reverseOrder());
+      results.add(index + 1);
 
-      // Element 0 is a special case. Is the player rank 1?
-      // if yes, then break
-      if (rankedLeaderBoard[0].equals(player.get(i))) {
-        results.add(1);
-      } else {
-        // Inspect all the remaining scores and allocate ranks starting from element 1
-        for (int j = 1; j < rankedLeaderBoard.length; j++) {
-          // Compare the score with the previous
-          // if the scores is less, than this is a new rank
-          if (rankedLeaderBoard[j] < rankedLeaderBoard[j - 1]) {
-            rankCounter++;
-          }
-
-          // is this a rank we care about?
-          if ((rankedLeaderBoard[j].equals(player.get(i))) && !found) {
-            results.add(rankCounter);
-            found = true;
-          }
-        }
-      }
+      rankedLeaderBoard.remove(index);
     }
 
     return results;
