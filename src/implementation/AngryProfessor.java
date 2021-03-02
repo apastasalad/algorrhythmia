d71bcae18ac742f7688e165c5bff145e06bc545f
Craig Fothers <46976302+apastasalad@users.noreply.github.com>
2021-03-02 16:19:43 +1100

    AngryProfessor algorithm update with passing tests

--- src/implementation/AngryProfessor.java
+++ src/implementation/AngryProfessor.java
@@ -2,12 +2,23 @@ package implementation;
 
 public class AngryProfessor {
 
-  // Complete the angryProfessor function below.
+  // Is the class on today?
   static String angryProfessor(int k, int[] a) {
 
+    int onTimeCounter = 0;
 
-    return "FAIL";
+    // Look at the arrival times, count the number of on-time students.
+    for (int arrivalTime : a) {
+      if (arrivalTime <= 0) {
+        onTimeCounter++;
+      }
+    }
 
+    // is the class cancelled today
+    if (onTimeCounter >= k) {
+      return "NO"; // class is not cancelled
+    } else {
+      return "YES"; // class is cancelled
+    }
   }
-
 }
